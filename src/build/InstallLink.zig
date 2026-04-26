ab2f780668bda9c8d018a015b4af1ea7d2dcfb08
Mike Will <myQwil@gmail.com>
2026-04-26 12:44:54 -0400

    fix InstallLink

--- src/build/InstallLink.zig
+++ src/build/InstallLink.zig
@@ -37,43 +37,53 @@ pub fn create(
 	return link;
 }
 
+fn fullPath(b: *Build, path: []const u8) []const u8 {
+	if (std.fs.path.isAbsolute(path)) {
+		return path;
+	}
+	defer b.allocator.free(path);
+	return b.build_root.join(b.allocator, &.{ path })
+		catch @panic("unhandled error");
+}
+
 fn make(step: *Step, _: Step.MakeOptions) !void {
 	const b = step.owner;
 	const io = b.graph.io;
+	const gpa = b.allocator;
 	const link: *InstallLink = @fieldParentPtr("step", step);
 
-	const dest = b.getInstallPath(link.dir, link.dest_rel_path);
-	const head = std.fs.path.dirname(dest).?;
+	const dest = fullPath(b, b.getInstallPath(link.dir, link.dest_rel_path));
+	defer gpa.free(dest);
+
+	const head = std.fs.path.dirname(dest) orelse ".";
+	const tail = std.fs.path.basename(dest);
 
 	// dest folder must already exist before attempting to put symlinks in it
-	var dir: std.Io.Dir = blk: { if (std.fs.path.isAbsolute(head)) {
-		var root = try std.Io.Dir.openDirAbsolute(io, "/", .{});
-		defer root.close(io);
-		root.access(io, head[1..], .{}) catch root.createDirPath(io, head[1..]) catch {};
-		break :blk try std.Io.Dir.openDirAbsolute(io, head, .{});
-	} else {
+	var dir: std.Io.Dir = blk: {
 		const cwd = std.Io.Dir.cwd();
 		cwd.access(io, head, .{}) catch cwd.createDirPath(io, head) catch {};
 		break :blk try cwd.openDir(io, head, .{});
-	}};
+	};
 	defer dir.close(io);
 
-	const target_path = blk: {
+	const target = blk: {
 		const p = try link.source.getPath4(b, step);
-		const full_src_path = b.pathResolve(&.{ p.root_dir.path orelse ".", p.sub_path });
-		if (std.Io.Dir.path.relative(b.allocator, ".", null, head, full_src_path)) |rel| {
-			b.allocator.free(full_src_path);
+		const src_path = fullPath(b,
+			b.pathResolve(&.{ p.root_dir.path orelse ".", p.sub_path }));
+		if (std.Io.Dir.path.relative(gpa, ".", null, head, src_path)) |rel| {
+			gpa.free(src_path);
 			break :blk rel;
 		} else |_| {
-			break :blk full_src_path;
+			break :blk src_path;
 		}
 	};
-	defer b.allocator.free(target_path);
+	defer gpa.free(target);
+	// std.debug.print("{s} -> {s}\n", .{ dest, target });
 
-	dir.symLink(io, target_path, dest[head.len+1..], .{}) catch |err| switch (err) {
+	dir.symLink(io, target, tail, .{}) catch |err| switch (err) {
 		error.PathAlreadyExists => {},
 		else => return step.fail("unable to install symlink '{s}' -> '{s}': {s}",
-			.{ link.dest_rel_path, target_path, @errorName(err) }),
+			.{ link.dest_rel_path, target, @errorName(err) }),
 	};
 }
 
